b5bde3c3756e2a2f2e08363e935db91a377def55
richasinhaa <richa.sinha0603@gmail.com>
2022-01-29 20:33:34 +0800

    API Template for Zoomcar Clone added

---
+++ src/main/java/com/api/zoomcarclone/ZoomcarCloneApplication.java
@@ -0,0 +1,13 @@
+package com.api.zoomcarclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomcarCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomcarCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/zoomcarclone/ZoomcarCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.zoomcarclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomcarCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
